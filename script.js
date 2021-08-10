147f63b997cd2e038ad4fa4c17fc5deeabc5e934
julian020914 <demianflithxd.14@gmail.com>
2021-08-10 14:35:18 -0500

    final

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 //funcionalidad del boton
-document.getElementById("botonArriba").addEventListener("click", scrollUp);
-
-function scrollUp(){
-
-  var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
-    if (currentScroll > 0){
-        window.scrollTo (0, 0);
+document.getElementById("botonArriba").addEventListener("click", irArriba);
+function irArriba(){
+  let desplazamiento = document.documentElement.scrollTop || document.body.scrollTop;
+  if (desplazamiento > 0) {
+    window.scrollTo (0, 0);
     }
+    }
+
+botonSubir = document.getElementById("botonArriba");
+
+window.onscroll = function(){
+
+    var scroll = document.documentElement.scrollTop;
+
+    if (scroll > 500){
+        botonSubir.style.transform = "scale(1)";
+    }else if(scroll < 500){
+        botonSubir.style.transform = "scale(0)";
+    }
+
 }
-
-
-///
-
